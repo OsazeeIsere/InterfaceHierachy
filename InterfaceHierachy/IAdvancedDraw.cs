c2f616a30271d63101023500f7947d3dfb568d2d
Osazee Isere <oz1.concepts@gmail.com>
2021-06-25 04:01:58 +0100

    initial commit

---
+++ InterfaceHierachy/IAdvancedDraw.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierachy
+{
+    interface IAdvancedDraw:IDrawable
+    {
+        void DrawInBoundingBox(int top, int left, int bottom, int right);
+        void DrawUpSideDown();
+            
+    }
+}
--- InterfaceHierachy/Program.cs
+++ InterfaceHierachy/Program.cs
@@ -10,6 +10,24 @@ namespace InterfaceHierachy
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Simple Interface Hierachy");
+            //call from object level
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpSideDown();
+
+            //get IAdvancedDraw explicitly
+            IAdvancedDraw IAdvDraw = myBitmap as IAdvancedDraw;
+            if (IAdvDraw != null)
+            {
+                IAdvDraw.DrawUpSideDown();
+            }
+            Console.ReadLine();
+
         }
+
+
+        
     }
 }
